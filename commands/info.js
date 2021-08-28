1a1f01f69c18bb78535118d03b1916a30f1dea79
lorie1 <87571590+lorie1@users.noreply.github.com>
2021-08-28 20:38:31 +0200

    info
    info

--- commands/info.js
+++ commands/info.js
@@ -4,11 +4,11 @@ module.exports.run = async (client, message, args) => {
 
     var botEmbed = new discord.MessageEmbed()
             .setTitle('Informatie')
-            .setDescription(`Tilburg surveillance dienst is een server om surveillance te doen, dit werkt zo:\nMocht er een surveillance komen word dat eerst gemeld in het surveillance kanaal, als je mee wild doen moet u de brievings call joinen anders kunt u niet mee doen!\n\n **_verdere informatie te vinden in uitleg._**`)
+            .setDescription(`Tilburg V1 is een RP sevrer moemtneel is de game nog niet uit, wij zijn er wel druk mee bezig.\nHet is nog niet bekend wanneer de game uit zal komen!`)
             .setColor("#00f518")
             .addField("Bot naam", client.user.username);
 
-            message.channel.send("Alle commands staan in je privé berichten! :mailbox_with_mail:")
+            message.channel.send("Alle infotmatie staat in je privé berichten! :mailbox_with_mail:")
             return message.author.send(botEmbed);
 }      
 
