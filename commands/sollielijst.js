6a95f4414da4e20fe4062c26e563ad857a204d58
lorie1 <87571590+lorie1@users.noreply.github.com>
2021-08-29 20:23:48 +0200

    d

--- commands/sollielijst.js
+++ commands/sollielijst.js
@@ -4,13 +4,13 @@ module.exports.run = async (client, message, args) => {
 
     const categoryID = "880254056524836925";
 
-    if (message.member.role(`881316715307339806`)) return message.reply("Jij kan dit niet doen");
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Jij kan dit niet doen");
 
     if (message.channel.parentID == categoryID) {
 
         var serverEmbed = new discord.MessageEmbed()
             .setTitle(`Sollicietatie lijst:`)
-            .setDescription("Wat is je leeftijd?\n\nWat is uw roblox naam?\n\nWat is uw discord naam+tag?\n\nVoor welke eenheid wild u solliciteren?\n\nWat is AA?\n\nWat is FRP?\n\nWat doet u als u een hogere rank FRP ziet doen?\n\nWat is Prio 1?\n\nWat is Prio 2?\n\nWat is prio 3?\n\nWat is prio 5?\n\nWat doe je bij een BTGV?\n\nHoe geeft u een training(leg uit)?\n\nHoevaak bent u online?\n\nWat doet u als een burger FRP doet?\n\nHeeft u ervaring met het HR+ werk?\n\nHoe vaak per week bent u online?\n\nKunt u het leden aantal verhogen?")
+            .setDescription("Wat is je leeftijd?\n\nWat is uw roblox naam?\n\nWat is uw discord naam+tag?\n\nVoor welke eenheid wild u solliciteren?\n\nWat is AA?\n\nWat is FRP?\n\nWat doet u als u een hogere rank FRP ziet doen?\n\nWat is Prio 1?\n\nWat is Prio 2?\n\nWat is prio 3?\n\nWat is prio 5?\n\nWat doe je bij een BTGV?\n\nHoe geeft u een training(leg uit)?\n\nHoevaak bent u online?\n\nWat doet u als een burger FRP doet?\n\nHeeft u ervaring met het HR+ werk?\n\nHoe vaak per week bent u online?\n\nKunt u het leden aantal versterken?")
             .setColor("#kleur")
 
 
