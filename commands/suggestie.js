49bf35e9ea04882ce9ec74af85c92c60fc1bbe7f
lorie1 <87571590+lorie1@users.noreply.github.com>
2021-08-30 18:16:06 +0200

    suggestie update

--- commands/suggestie.js
+++ commands/suggestie.js
@@ -2,7 +2,31 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    const channel = message.guild.channels.cache.find
+    const channel = message.guild.channels.cache.find(ch => ch.name === "suggestie");
+    if(!channel) return message.replay("Kannaal niet gevonden.");
+
+    var argsBericht = args.join(" ");
+    if(!argsBericht) return message.replay("Gelieven een suggestie mee te geven!");
+
+    var embed = new discord.MessageEmbed()
+        .setTitle(`Suggestie ${message.author.tag}`)
+        .setDescription(argsBericht)
+        .setColor("#ffcc00")
+        .setFooter(time)
+
+
+    channel.send(embed).then(async (msg) => {
+
+       await msg.react("👍");
+       await msg.react("👎");
+       message.delete();
+    }).cache(err => {
+        console.log(err);
+    });     
+
+
+
+
 
 }
 
