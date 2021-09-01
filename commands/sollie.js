d7f5d2f3919336039905a7e773ac72772c0a60fa
lorie1 <87571590+lorie1@users.noreply.github.com>
2021-09-01 09:35:11 +0200

    et4r

--- commands/sollie.js
+++ commands/sollie.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
 
     var serverEmbed = new discord.MessageEmbed()
             .setTitle("Sollicitaties")
-            .setDescription("Politie: gesloten\n\nBrandweer: gesloten\n\nAmbulance: gesloten\n\nnormaal staff: gesloten\n\nDeveloper: gesloten\n\n\n**_Alle sollicitatsies zijn gesloten_**")
+            .setDescription("Politie: open\n\nBrandweer: open\n\nAmbulance: open\n\nnormaal staff: open\n\nDeveloper: gesloten\n\n\n**_Alle sollicitatsies zijn gesloten_**")
             .setColor("#0b03fc")
             .setTimestamp();
 
